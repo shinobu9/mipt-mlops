f772e7d5052f64db735fd21a199a1d2b67cec145
Lisa Kamenskaya <kamelis113@gmail.com>
2023-12-03 22:33:33 +0300

    updated project

---
+++ mipt-mlops/infer.py
@@ -0,0 +1,35 @@
+import itertools
+
+import numpy as np
+import torch
+from torch.utils.data import DataLoader
+
+from constants import BATCH_SIZE_DEFAULT, data_dir, models_dir, predictions_dir
+from datasets import Data
+from networks import NeuralNetwork
+
+
+if __name__ == "__main__":
+    with open(data_dir / "test.npy", "rb") as stream:
+        X_test = np.load(stream)
+        y_test = np.load(stream)
+    test_data = Data(X_test, y_test)
+    test_dataloader = DataLoader(test_data, BATCH_SIZE_DEFAULT, shuffle=True)
+    model = NeuralNetwork()
+    model.load_state_dict(torch.load(models_dir / "circle_network.pkl"))
+
+    y_pred = []
+    total = 0
+    correct = 0
+    with torch.no_grad():
+        for X, y in test_dataloader:
+            outputs = model(X)
+            predicted = np.where(outputs < 0.5, 0, 1)
+            predicted = list(itertools.chain(*predicted))
+            y_pred.append(predicted)
+            total += y.size(0)
+            correct += (predicted == y.numpy()).sum().item()
+    result = np.array(list(itertools.chain(*y_pred)))
+    result.tofile(predictions_dir / "predict.csv", sep=",")
+
+    print(f"Accuracy of the network: {100 * correct // total}%")
